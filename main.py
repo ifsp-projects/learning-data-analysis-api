621f9a99680f11cc655281b00f4f1f05a7b18bb6
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-07-13 23:46:04 -0300

    chore(api) initial project python api config

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/getUserAnalysis/<user_id>")
+def getBaseStudentAnalysis():
+  return 'Analysis result :D'
+
+if __name__ == '__main__':
+  app.run(debug=True)
